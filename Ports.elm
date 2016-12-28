83eaff9af86bec1e6f69f6059c4ece29ce9f2373
Lisardo <lisardo.kist@gmail.com>
2016-12-28 15:05:07 -0200

    first commit

---
+++ Ports.elm
@@ -0,0 +1,16 @@
+-- Ports.elm
+
+
+port module Ports exposing (..)
+
+
+type alias ImagePortData =
+    { contents : String
+    , filename : String
+    }
+
+
+port fileSelected : String -> Cmd msg
+
+
+port fileContentRead : (ImagePortData -> msg) -> Sub msg
